65cb37ace2f74045709b453cfd89434c509b3fe9
Shigma <shigma10826@gmail.com>
2023-02-10 00:30:26 +0800

    chore: add basic template

---
+++ template/src/index.console.ts
@@ -0,0 +1,18 @@
+import { Context, Schema } from 'koishi'
+import { resolve } from 'path'
+import {} from '@koishijs/plugin-console'
+
+export const name = '{{name}}'
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({})
+
+export function apply(ctx: Context) {
+  ctx.using(['console'], (ctx) => {
+    ctx.console.addEntry({
+      dev: resolve(__dirname, '../client/index.ts'),
+      prod: resolve(__dirname, '../dist'),
+    })
+  })
+}
