e5f4c76b75645cfed9cbe77ac61dafb19e37eb3f
多玩幻灵qwq <94176676+HuanLinOTO@users.noreply.github.com>
2023-07-10 15:02:52 +0800

    fix(setup): fatel error on windows about git init commit

--- packages/scripts/src/setup.ts
+++ packages/scripts/src/setup.ts
@@ -1,6 +1,6 @@
 import { CAC } from 'cac'
 import { copyFile, mkdir, readFile, readJson, writeFile } from 'fs-extra'
-import { execSync } from 'child_process'
+import { execSync, spawn } from 'child_process'
 import { resolve } from 'path'
 import { cwd, meta } from '.'
 import { blue, red } from 'kleur'
@@ -133,9 +133,9 @@ class Initiator {
       copyFile(this.source + '/.gitattributes', this.target + '/.gitattributes'),
       copyFile(this.source + '/_gitignore', this.target + '/.gitignore'),
     ])
-    execSync('git init', { cwd: this.target, stdio: 'ignore' })
-    execSync('git add .', { cwd: this.target, stdio: 'ignore' })
-    execSync('git commit -m "initial commit"', { cwd: this.target, stdio: 'ignore' })
+    spawn('git', ['init'], { cwd: this.target, stdio: 'ignore' });
+    spawn('git', ['add', '.'], { cwd: this.target, stdio: 'ignore' });
+    spawn('git', ['commit', '-m', '"initial commit"'], { cwd: this.target, stdio: 'ignore' });
   }
 }
 
